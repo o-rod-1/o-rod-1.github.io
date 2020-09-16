446ea1c3e8376a8f15d2ea6a5c9d7eca3795be07
Oscar Rodriguez <oscar.rodriguez@vonage.com>
2020-09-16 14:55:09 -0700

    config

--- Basic Video Chat/js/config.js
+++ Basic Video Chat/js/config.js
@@ -4,12 +4,12 @@
 // Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
 // Do not include the trailing slash. See the README for more information:
 
-var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+var SAMPLE_SERVER_BASE_URL = 'https://o-rod-1.github.io';
 
 // OR, if you have not set up a web server that runs the learning-opentok-php code,
 // set these values to OpenTok API key, a valid session ID, and a token for the session.
 // For test purposes, you can obtain these from https://tokbox.com/account.
 
-var API_KEY = '';
+var API_KEY = '46923354';
 var SESSION_ID = '';
 var TOKEN = '';
